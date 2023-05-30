a7c26fee22cf1053a702c6bb476940a8d209a740
Cameron Sheppard <websitesbycss@gmail.com>
2023-05-30 09:47:28 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,12 @@
+const css = document.querySelector("h3");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const body = document.getElementById("gradient");
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
